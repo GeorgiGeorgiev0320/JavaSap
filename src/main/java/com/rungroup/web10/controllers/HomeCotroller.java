01487cf43c745e5baeae87e20bca373c64d2e992
GeorgiGeorgiev0320 <georgi.georgiev0320@gmail.com>
2024-04-26 13:55:11 +0300

    FirstPage

---
+++ src/main/java/com/rungroup/web10/controllers/HomeCotroller.java
@@ -0,0 +1,13 @@
+package com.rungroup.web10.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeCotroller {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/products";
+    }
+}
